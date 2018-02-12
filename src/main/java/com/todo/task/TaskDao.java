a5b2643f6654d637b7ebe76343b4c1c7534174aa
Mateusz Parkitny <parkitny.mateusz@gmail.com>
2018-02-12 18:49:51 +0100

    added usermap and taskdao

--- src/main/java/com/todo/task/TaskDao.java
+++ src/main/java/com/todo/task/TaskDao.java
@@ -1,24 +1,27 @@
-package com.todo.user;
+package com.todo.task;
+
+import com.todo.user.User;
+import com.todo.user.UserMapper;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-public class UserDao {
+public class TaskDao {
 
-    public static final String SELECT_FROM_USERS_WHERE_LOGIN_S = "select * from users where login = '%s'";
+    public static final String SELECT_FROM_USERS_WHERE_LOGIN_S = "select * from tasks where task_name = '%s'";
     private Connection connection;
     private UserMapper mapper;
 
-    public UserDao(Connection connection) {
+    public TaskDao(Connection connection) {
         this.connection = connection;
         this.mapper = new UserMapper();
     }
 
-    public User getByLogin(String login) throws SQLException {
+    public User getByTask(String task) throws SQLException {
         ResultSet res = connection.createStatement().executeQuery(
-                String.format(SELECT_FROM_USERS_WHERE_LOGIN_S, login)
+                String.format(SELECT_FROM_USERS_WHERE_LOGIN_S, task)
         );
         if(!res.next()) throw new NoSuchElementException();
         return mapper.getSingle(res);
---
+++ src/main/java/com/todo/user/UserMapper.java
@@ -0,0 +1,24 @@
+package com.todo.user;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserMapper {
+    public static final int ID = 1;
+    public static final int LOGIN = 2;
+    public static final int PASSWORD = 3;
+
+    public User getSingle (ResultSet res) throws SQLException{
+        List<User> users = getList(res);
+        if (users.size() != 1) throw new IllegalStateException();
+        return users.get(0);
+    }
+    public List<User> getList (ResultSet res) throws SQLException{
+     List<User> users = new ArrayList<>();
+     while (res.next())
+      users.add(new User(res.getInt(ID), res.getString(LOGIN), res.getString(PASSWORD)));
+    return users;
+    }
+}
